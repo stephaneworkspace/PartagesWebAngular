6e0064321fcf37512a37c377643e7a6f2dc1baf9
Stéphane <s.bressani@bluewin.ch>
2019-01-03 02:38:01 +0100

    backup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faCoffee, faDesktop } from '@fortawesome/free-solid-svg-icons';
+import { SizeProp } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PartagesWebAngular';
+  faCoffee = faCoffee;
+  faDesktop = faDesktop;
+  public faSizeProp: SizeProp;
+  public faSpinProp: boolean; // on mouse over true
+
+  ngOnInit() {
+    this.faSizeProp = '5x';
+    this.faSpinProp = false;
+  }
 }
