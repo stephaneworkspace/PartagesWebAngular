6def65e0e37bc592ca586bcb92355c572730559d
Bressani <Steee-Fx@hotmail.com>
2019-02-01 12:50:49 +0100

    backup

--- src/app/admin/gestion-pages/nouveau-section/nouveau-section.component.ts
+++ src/app/admin/gestion-pages/nouveau-section/nouveau-section.component.ts
@@ -1,22 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-/*
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';*/
-/*import {
-  faEdit,
-  faDesktop,
-  faMusic,
-  faGlobe,
-  faPuzzlePiece,
-  faSitemap,
-  faCaretSquareDown,
-  faFileAlt,
-  faTrashAlt,
-  faLevelUpAlt,
-  faLevelDownAlt } from '@fortawesome/free-solid-svg-icons';*/
+import { SizeProp } from '@fortawesome/fontawesome-svg-core';
+
 @Component({
   selector: 'app-nouveau-section',
   templateUrl: './nouveau-section.component.html',
@@ -24,24 +8,12 @@ import { fab } from '@fortawesome/free-brands-svg-icons';*/
 })
 export class NouveauSectionComponent implements OnInit {
   model: any = {};
-  /*
-  // Chargement de toutes les icones
-  faEdit = faEdit;
-  faPuzzlePiece = faPuzzlePiece;
-  faDesktop = faDesktop;
-  faMusic = faMusic;
-  faGlobe = faGlobe;
-  faSitemap = faSitemap;
-  faCaretSquareDown = faCaretSquareDown;
-  faFileAlt = faFileAlt;
-  faTrashAlt = faTrashAlt;
-  faLevelUpAlt = faLevelUpAlt;
-  faLevelDownAlt = faLevelDownAlt;*/
 
+  public faSizeProp: SizeProp;
   constructor() { }
 
   ngOnInit() {
-    // library.add(fas, far, fab);
+    this.faSizeProp = 'lg';
   }
 
 }
