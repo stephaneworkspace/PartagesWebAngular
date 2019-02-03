0feaecce1314a60ad63778c282b1c5292d6801e9
Bressani <Steee-Fx@hotmail.com>
2019-02-03 07:50:37 +0100

    backup ne fonctionne pas

--- src/app/admin/gestion-pages/nouveau-article/nouveau-article.component.ts
+++ src/app/admin/gestion-pages/nouveau-article/nouveau-article.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { marked } from 'marked';
 
 @Component({
   selector: 'app-nouveau-article',
   templateUrl: './nouveau-article.component.html',
   styleUrls: ['./nouveau-article.component.scss']
 })
-export class NouveauArticleComponent implements OnInit {
-  model: any = {};
+export class NouveauArticleComponent implements OnInit, OnChanges {
+  @Input() model: any = {};
+  markedArticle: string;
 
   constructor() { }
 
@@ -14,6 +16,15 @@ export class NouveauArticleComponent implements OnInit {
     this.model.sectionSelect = 'Hors-ligne';
     this.model.titreSelect = 'Hors-ligne';
     this.model.sousTitreSelect = 'Hors-ligne';
+  }
+
+
+  ngOnChanges(changes: SimpleChanges) {
+    // if (changes['model.article']) {
+      const md = marked.setOptions({});
+      this.markedArticle = md.parse(this.model.article);
+      this.markedArticle = this.model.article;
+    // }
   }
 
 }
