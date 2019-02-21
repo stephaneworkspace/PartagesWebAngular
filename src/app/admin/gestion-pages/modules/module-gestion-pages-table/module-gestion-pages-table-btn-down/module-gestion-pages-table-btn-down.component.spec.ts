de406196390cd92e9ebe43b2bc5eb9ffc4773a36
stephaneworkspace <Steee-Fx@hotmail.com>
2019-02-21 14:42:44 +0100

    ok

--- src/app/admin/gestion-pages/modules/module-gestion-pages-table/module-gestion-pages-table-btn-down/module-gestion-pages-table-btn-down.component.spec.ts
+++ src/app/admin/gestion-pages/modules/module-gestion-pages-table/module-gestion-pages-table-btn-down/module-gestion-pages-table-btn-down.component.spec.ts
@@ -1,7 +1,4 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { ModuleGestionPagesTableBtnDownComponent } from './module-gestion-pages-table-btn-down.component';
 
