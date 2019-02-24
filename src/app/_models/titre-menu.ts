1ddb5eda8dcd66583a1d8a60d0de995345ddcc91
stephaneworkspace <s.bressani@bluewin.ch>
2019-02-24 22:05:01 +0100

    backup

--- src/app/_models/titre-menu.ts
+++ src/app/_models/titre-menu.ts
@@ -4,6 +4,7 @@ export interface TitreMenu {
   id: number;
   sectionId?: number;
   nom: string;
+  swHorsLigne?: boolean; // Dto pour créer depuis front end, le back end ne retourne pas ce champ, il faut analiser sectionId
   position?: number;
   swMouseOver?: boolean; // Champ virtuel
 }
