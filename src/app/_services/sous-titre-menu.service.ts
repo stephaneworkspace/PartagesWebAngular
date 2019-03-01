ef72fb80d1caf7c83cc826f6db02ae942a779e84
stephaneworkspace <s.bressani@bluewin.ch>
2019-03-01 16:42:04 +0100

    correction erreur

--- src/app/_services/sous-titre-menu.service.ts
+++ src/app/_services/sous-titre-menu.service.ts
@@ -19,26 +19,26 @@ export class SousTitreMenuService {
   constructor(private http: HttpClient) {}
 
   getSousTitreMenu(id: number): Observable<SousTitreMenu> {
-    return this.http.get<SousTitreMenu>(this.baseUrl + 'SousTitresMenus/' + id, httpOptions);
+    return this.http.get<SousTitreMenu>(this.baseUrl + 'SousTitreMenus/' + id, httpOptions);
   }
 
   update(id: number, item: SousTitreMenu) {
-    return this.http.put(this.baseUrl + 'SousTitresMenus/' + id, item);
+    return this.http.put(this.baseUrl + 'SousTitreMenus/' + id, item);
   }
 
   create(item: SousTitreMenu) {
-    return this.http.post(this.baseUrl + 'SousTitresMenus', item);
+    return this.http.post(this.baseUrl + 'SousTitreMenus', item);
   }
 
   delete(id: number) {
-    return this.http.delete(this.baseUrl + 'SousTitresMenus/' + id, httpOptions);
+    return this.http.delete(this.baseUrl + 'SousTitreMenus/' + id, httpOptions);
   }
 
   up(id: number) {
-    return this.http.post(this.baseUrl + 'SousTitresMenus/up/' + id, {}, httpOptions);
+    return this.http.post(this.baseUrl + 'SousTitreMenus/up/' + id, {}, httpOptions);
   }
 
   down(id: number) {
-    return this.http.post(this.baseUrl + 'SousTitresMenus/down/' + id, {}, httpOptions);
+    return this.http.post(this.baseUrl + 'SousTitreMenus/down/' + id, {}, httpOptions);
   }
 }
