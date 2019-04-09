8694ec41cdf1ff0b22e33093cb0d355e0d52c34f
stephaneworkspace <s.bressani@bluewin.ch>
2019-04-09 14:57:19 +0200

    avancée

--- src/app/_services/forum/forum-poste.service.ts
+++ src/app/_services/forum/forum-poste.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { ForumPoste } from 'src/app/_models/Forum/forum-poste';
 import { Observable } from 'rxjs';
+import { PaginatedResult } from 'src/app/_reponse/pagination';
+import { map } from 'rxjs/operators';
 
 const httpOptions = {
   headers: new HttpHeaders({
     'Authorization': 'Bearer ' + localStorage.getItem('token')
   })
 };
+
+// Pour un ensemble déjà fermé
+const headers_fix = new HttpHeaders({
+  'Authorization': 'Bearer ' + localStorage.getItem('token')
+});
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +26,26 @@ export class ForumPosteService {
   constructor(private http: HttpClient) {}
 
   // pagination à faire ici
-  getForumPoste(id: number, pageNumber: number): Observable<ForumPoste[]> {
-    return this.http.get<ForumPoste[]>(this.baseUrl + 'ForumPostes/' + id + '?PageNumber=' + pageNumber, httpOptions);
+  getForumPoste(id: number, page?, itemsPerPage?): Observable<PaginatedResult<ForumPoste[]>> {
+    const paginatedResult: PaginatedResult<ForumPoste[]> = new PaginatedResult<ForumPoste[]>();
+
+    let params = new HttpParams();
+
+    if (page != null) {
+      params = params.append('pageNumber', page);
+    }
+    if (itemsPerPage != null) {
+      params = params.append('pageSize', itemsPerPage);
+    }
+    return this.http.get<ForumPoste[]>(this.baseUrl + 'ForumPostes/' + id, { headers: headers_fix, observe: 'response', params })
+    .pipe(
+      map(response => {
+        paginatedResult.result = response.body;
+        if (response.headers.get('Pagination') != null) {
+          paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'))
+        }
+        return paginatedResult;
+      })
+    );
   }
 }
