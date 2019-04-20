873aad5b4a9f7c85e67f1404c976eb4dbf10bc12
stephaneworkspace <s.bressani@bluewin.ch>
2019-04-20 17:46:23 +0200

    backup avancé

--- src/app/_modules/div/forum/reponse/reponse.component.ts
+++ src/app/_modules/div/forum/reponse/reponse.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ForumSujet } from 'src/app/_models/Forum/forum-sujet';
 import * as moment from 'moment';
 import 'moment/locale/fr';
+import { FormError } from 'src/app/_class/form-error';
 
 @Component({
   selector: 'app-module-div-forum-reponse',
@@ -10,11 +11,16 @@ import 'moment/locale/fr';
 })
 export class ModuleDivForumReponseComponent implements OnInit {
   @Input() sujet: ForumSujet;
+  @Input() contenu?: string;
+  model: any = {};
+  formError: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.formError = new FormError();
     moment.locale('fr');
+    this.model.contenu = this.contenu;
   }
 
   dateFormatLL(date: Date){
@@ -23,6 +29,29 @@ export class ModuleDivForumReponseComponent implements OnInit {
 
   dateFormatLLLL(date: Date){
     return moment(date).format('LLLL');
+  }
+
+
+  submitForm() {
+    // Initialisation des erreurs précédantes
+    this.formError.clear();
+    /*
+    this.articleService.update(this.model.id, this.model).subscribe(next => {
+      this.alertify.success('Article &laquo;' + this.model.nom + '&raquo; mis à jour');
+      this.editForm.reset(this.model);
+      this.router.navigate(['/admin']);
+    }, error => {
+      // https://github.com/laracasts/Vue-Forms/blob/master/public/js/app.js
+      // Ensuite trouver moyen de traduire les messages d'erreurs
+      if (typeof error.error === 'string') {
+        this.alertify.error(error.error);
+      } else {
+        this.formError.record(error.error.errors);
+        Object.keys(this.formError.getAll()).forEach(element => {
+          this.alertify.error(this.formError.get(element));
+        });
+      }
+    });*/
   }
 
 }
