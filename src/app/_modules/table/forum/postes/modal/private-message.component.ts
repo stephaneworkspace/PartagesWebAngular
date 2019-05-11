f8739e4379df5318efb835c98e74f7619f015592
stephaneworkspace <s.bressani@bluewin.ch>
2019-05-11 13:49:06 +0200

    envoi de message fonctionne

--- src/app/_modules/table/forum/postes/modal/private-message.component.ts
+++ src/app/_modules/table/forum/postes/modal/private-message.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter, SimpleChange, OnChanges } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { DtoAdminGestionPagesTable as Dto } from 'src/app/_dto/admin/gestion-pages/table';
+import { MessagerieDtoInput as Dto } from 'src/app/_dto/messagerie/messagerie-dto-input';
 import { User } from 'src/app/_models/user';
 import { FormError } from 'src/app/_class/form-error';
 import { MessagerieService } from 'src/app/_services/messagerie.service';
@@ -21,6 +21,7 @@ export class ModuleTableForumPostesModalPrivateMessageComponent implements OnIni
 
   ngOnInit() {
     this.formError = new FormError();
+    this.model.UserId = this.item.id;
   }
 
   btnEnvoyerPush() {
@@ -29,9 +30,9 @@ export class ModuleTableForumPostesModalPrivateMessageComponent implements OnIni
 
     this.messagerieService.postEnvoiMessagerie(this.model).subscribe(next => {
       this.alertify.success('Message envoyé');
-      this.output.emit(/*{
-        item: this.item // A FAIRE DTO 9 mai
-      }*/);
+      this.output.emit();
+        //item: this.item // A FAIRE DTO 9 mai
+      // });
       // this.router.navigate(['/forum/sujet/' + this.model.forumCategorieId , { PageSize: 5, PageNumber: 1 } ]);
     }, error => {
       // https://github.com/laracasts/Vue-Forms/blob/master/public/js/app.js
